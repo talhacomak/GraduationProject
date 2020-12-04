499ba5bae8329054281cd9a7f90b434a2c446b0c
talhacomak <tcomak32@outlook.com>
2020-12-04 12:12:31 +0300

    user interface, requirements, login, register

--- dbinit.py
+++ dbinit.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     url = os.getenv("DATABASE_URL")
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
-        #sys.exit(1)
-    #initialize(db_url)
+        sys.exit(1)
+    initialize(db_url)
